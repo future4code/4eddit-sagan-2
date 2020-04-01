792236ad66ff6251fc05cfb724d563ee154957c4
ThalesMilanezi <milanezitf@gmail.com>
2020-04-01 15:24:59 -0300

    fazendo alinhamentos da pagina de login

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -2,54 +2,55 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 import { push } from "connected-react-router";
 import { routes } from "../../containers/Router/index";
-import {newRegister} from "../../actions/Actions"
-
+import { newRegister } from "../../actions/Actions"
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const userRegister = [
   {
-    name:'username', 
-    label:'Nome do novo Usuário',
-    type:'text', 
-    pattern:'[a-zA-Z]{3,}', 
-    title:'nome do usuário com no mínimo 3 letras'
+    name: 'username',
+    label: 'Nome do novo Usuário',
+    type: 'text',
+    pattern: '[a-zA-Z]{3,}',
+    title: 'nome do usuário com no mínimo 3 letras'
   },
   {
-    name:'email', 
-    label:'email do novo Usuário',
-    type:'email', 
-    title:'email completo do usuário'
+    name: 'email',
+    label: 'email do novo Usuário',
+    type: 'email',
+    title: 'email completo do usuário'
   },
   {
-    name:'password', 
-    label:'senha do novo Usuário',
-    type:'password', 
-    pattern:'{5,}', 
-    title:'nova senha com no mínimo 5 digitos'
+    name: 'password',
+    label: 'senha do novo Usuário',
+    type: 'password',
+    pattern: '{5,}',
+    title: 'nova senha com no mínimo 5 digitos'
   }
 ]
 
-class Register extends Component{
-  constructor(props){
+class Register extends Component {
+  constructor(props) {
     super(props)
-    this.state={
-      form:{}
+    this.state = {
+      form: {}
     }
   }
 
 
   handleInputnewUser = event => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
     this.setState({
-      form:{
-        ...this.state.form, 
-        [name]:value
+      form: {
+        ...this.state.form,
+        [name]: value
       }
     })
   }
 
   componentDidUpdate() {
     const token = localStorage.getItem("token")
-    if(token !== null) {
+    if (token !== null) {
       this.props.goToFeed();
     }
   }
@@ -61,14 +62,15 @@ class Register extends Component{
   }
 
   render() {
-    return(
+    return (
       <div>
         <form onSubmit={this.handleOnSubmit}>
-          {userRegister.map(field =>{
-            return(
+          {userRegister.map(field => {
+            return (
               <div>
                 <label>{field.label}</label>
-                <input
+                <TextField
+                  variant="outlined"
                   name={field.name}
                   type={field.type}
                   value={this.state.form[field.name]}
@@ -79,7 +81,7 @@ class Register extends Component{
               </div>
             )
           })}
-          <button type="submit">Cadastrar</button>
+          <Button type="submit" variant="contained">Cadastrar</Button>
         </form>
       </div>
     )
@@ -90,8 +92,8 @@ const mapStateToProps = (state) => ({
   user: state.posts.user
 })
 
-const mapDispatchToProps = (dispatch) =>({
-    goToFeed: () => dispatch(push(routes.root)),
-    newRegister: (form) => dispatch(newRegister(form))
+const mapDispatchToProps = (dispatch) => ({
+  goToFeed: () => dispatch(push(routes.root)),
+  newRegister: (form) => dispatch(newRegister(form))
 })
 export default connect(mapStateToProps, mapDispatchToProps)(Register)
