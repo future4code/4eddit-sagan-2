5db909d56fbb0ddaa18f5f8ffaa0abeac688f06f
EduCosta85 <59846713+EduCosta85@users.noreply.github.com>
2020-04-01 15:21:42 -0300

    header estilizado

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -2,6 +2,11 @@ import React, {Component} from "react";
 import {WrapperDiv} from './styled'
 import {connect} from "react-redux";
 import {setLogout, setUser} from '../../actions/Actions'
+import {StyledAppBar, WrapperDivLogo, WrapperIcon, WrapperUserData} from './styled'
+import RedditIcon from '@material-ui/icons/Reddit';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 
 class Header extends Component {
 
@@ -12,35 +17,49 @@ class Header extends Component {
         }
     }
     render() {
-        const logado = (
-            <div> {/* <Avatar src="https://picsum.photos/200"/> */}
-                <p>User: {
-                    this.props.user === null ? "" : this.props.user.username
-                }</p>
-                <p>Email: {
-                    this.props.user === null ? "" : this.props.user.email
-                }</p>
-                <p>{
-                    this.props.user.length
-                }</p>
-                <button onClick={
-                    this.props.setLogout
-                }>Logout</button>
-            </div>
+        const userdata = (
+            <WrapperUserData>
+                <WrapperUserData>
+                    <Button variant="outlined"
+                        size={'small'}
+                        color="primary"
+                        onClick={
+                            this.props.setLogout
+                    }>Logout</Button>
+                </WrapperUserData>
+                <div>
+                    <Typography variant={'subtitle2'}
+                        color={'textPrimary'}>User: {
+                        this.props.user === null ? "" : this.props.user.username
+                    }</Typography>
+                    <Typography variant={'subtitle2'}
+                        color={'textPrimary'}>Email: {
+                        this.props.user === null ? "" : this.props.user.email
+                    }</Typography>
+                </div>
+            </WrapperUserData>
         )
-        const deslogado = (
-            <div>
-                <p></p>
-            </div>
+        const logodata = (
+            <WrapperDiv>
+                <WrapperIcon>
+                    <RedditIcon fontSize={'large'}/>
+                </WrapperIcon>
+                <WrapperDivLogo>
+                    <Typography variant={'h5'}
+                        color={'textPrimary'}>4eddit</Typography>
+                    <Typography variant={'subtitle2'}
+                        color={'textPrimary'}>Turma 2 - Sagan</Typography>
+                </WrapperDivLogo>
+            </WrapperDiv>
         )
         return (
-            <WrapperDiv>
-                <div>
-                    <h1>Logo</h1>
-                </div>
+            <StyledAppBar elevation={0}>
                 {
-                this.props.user.length === 0 ? deslogado : logado
-            } </WrapperDiv>
+                this.props.user.length === 0 ? "" : logodata
+            }
+                {
+                this.props.user.length === 0 ? "" : userdata
+            } </StyledAppBar>
         );
     }
 }
